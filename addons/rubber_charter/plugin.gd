cc823630818d1e2df428ded8ad7c2f0ad94969b2
legole0 <legole0@proton.me>
2025-03-29 13:32:12 -0300

    Editor reads notes, fixes for possible future bugs and readability improvements

--- addons/rubber_charter/plugin.gd
+++ addons/rubber_charter/plugin.gd
@@ -2,17 +2,25 @@
 extends EditorPlugin
 
 var addon_path:String = get_script().get_path().get_base_dir()+"/"
-var editor_instance = load(addon_path+"scenes/Editor.tscn").instantiate()
+var editor_path:String = addon_path+"Editor.tscn"
+var editor_instance:ChartEditor
 
 func _enter_tree() -> void:
 	if !Engine.is_editor_hint():
 		return
 	
+	editor_instance = ResourceLoader.load(editor_path).instantiate()
+	editor_instance.visible = false
+	editor_instance.active = true
 	EditorInterface.get_editor_main_screen().add_child(editor_instance)
+	connect("main_screen_changed", _main_screen_changed)
 
 func _exit_tree() -> void:
 	editor_instance.queue_free()
 	queue_free()
+
+func _main_screen_changed(screen_name:String):
+	editor_instance.visible = screen_name == _get_plugin_name()
 
 func _has_main_screen() -> bool:
 	return true
--- addons/rubber_charter/rulesets/mania/Mania.gd
+++ addons/rubber_charter/rulesets/mania/Mania.gd
@@ -8,5 +8,9 @@ func _setup_bar_lines(charts:Array[IndividualChart]):
 	for chart:IndividualChart in charts:
 		var bar_line:EditorManiaBarLine = bar_line_template.instantiate()
 		bar_line.name = chart.Name
-		bar_line._load_notes_from_chart(chart)
-		add_child(bar_line)
+		bar_line._load_chart(chart)
+		bar_line_container.add_child(bar_line)
+	
+	var first_bar_line:EditorManiaBarLine = bar_line_container.get_child(0)
+	var separation:int = first_bar_line.note_size.x# * first_bar_line.lanes
+	bar_line_container.add_theme_constant_override("separation", separation)
